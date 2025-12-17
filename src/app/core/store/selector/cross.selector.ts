fa31755abddddf5b14927d71cf05a3040754303a
RyanDevGitHub <ryanhaddad12@gmail.com>
2025-12-17 23:38:08 +0100

    add log in cross selectore

--- src/app/core/store/selector/cross.selector.ts
+++ src/app/core/store/selector/cross.selector.ts
@@ -6,7 +6,6 @@ import { IArtist } from '../../interfaces/user';
 import { selectAllAlbums } from './album.selector';
 import { selectAllArtists } from './artist.selector';
 import { selectAllSongs } from './song.selector';
-
 export const selectSearchResults = (searchTerm: string) =>
   createSelector(
     selectAllSongs,
@@ -15,23 +14,39 @@ export const selectSearchResults = (searchTerm: string) =>
     (songs: ISong[], albums: IAlbum[], artists: IArtist[]) => {
       const lowerTerm = searchTerm.toLowerCase();
 
-      const filteredSongs = songs.filter((song) =>
-        song.title.toLowerCase().includes(lowerTerm),
-      );
+      const filteredSongs = songs.filter((song) => {
+        if (!song || !song.title) {
+          console.error("🚨 ERREUR: Cette musique n'a pas de titre :", song);
+          return false;
+        }
+        return song.title.toLowerCase().includes(lowerTerm);
+      });
 
-      const filteredAlbums = albums.filter((album) =>
-        album.title.toLowerCase().includes(lowerTerm),
-      );
-      const filteredArtist = artists.filter((artist) =>
-        artist.firstName.toLowerCase().includes(lowerTerm),
-      );
+      const filteredAlbums = albums.filter((album) => {
+        if (!album || !album.title) {
+          console.error("🚨 ERREUR: Cet album n'a pas de titre :", album);
+          return false;
+        }
+        return album.title.toLowerCase().includes(lowerTerm);
+      });
+
+      const filteredArtist = artists.filter((artist) => {
+        if (!artist || !artist.firstName) {
+          console.error(
+            "🚨 ERREUR: Cet artiste n'a pas de firstName :",
+            artist
+          );
+          return false;
+        }
+        return artist.firstName.toLowerCase().includes(lowerTerm);
+      });
 
       return {
         songs: filteredSongs,
         albums: filteredAlbums,
         artists: filteredArtist,
       };
-    },
+    }
   );
 export const selectArtistData = (userId: string) =>
   createSelector(
@@ -53,7 +68,7 @@ export const selectArtistData = (userId: string) =>
             .sort(
               (a, b) =>
                 new Date(b.createdAt).getTime() -
-                new Date(a.createdAt).getTime(),
+                new Date(a.createdAt).getTime()
             )[0].cover
         : null;
 
@@ -63,5 +78,5 @@ export const selectArtistData = (userId: string) =>
         albums: artistAlbums,
         lastAlbumCover,
       };
-    },
+    }
   );
